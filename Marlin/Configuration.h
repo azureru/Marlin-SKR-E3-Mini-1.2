a6baea7cceb25ec84a8100a62f359494543cfe67
azureru <erwin@lumio.id>
2020-01-10 02:08:03 +0700

    Add PID calibration result

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -530,9 +530,14 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  // #define DEFAULT_bedKp 10.00
+  // #define DEFAULT_bedKi .023
+  // #define DEFAULT_bedKd 305.4
+
+  // hasil `M303 E-1 S60 C10` manual
+  #define DEFAULT_bedKp 57.63
+  #define DEFAULT_bedKi 11.23
+  #define DEFAULT_bedKd 197.10  
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
